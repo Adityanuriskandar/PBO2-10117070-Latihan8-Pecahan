c6872e25c8a570c324f01c6c06c45e95358a89de
Aditya Nur Iskandar <43404691+Adityanuriskandar@users.noreply.github.com>
2018-09-23 12:22:12 +0700

    Add files via upload

---
+++ PBO2-10117070-Latihan8-Pecahan/src/pbo2/pkg10117070/latihan8/pecahan/PBO210117070Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117070.latihan8.pecahan;
+
+/**
+ *
+ * @author Aditya
+ * NAMA     : Aditya Nur Iskandar
+ * KELAS    : PBO2/IF2
+ * NIM      : 10117070
+ * Deskripsi Program : Program ini berisi program untuk mengetahui 
+ * tipe data pecahan.
+ */
+public class PBO210117070Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789f;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float\t:" + f);
+        System.out.println("Double\t:" + d);
+    }
+    
+}
